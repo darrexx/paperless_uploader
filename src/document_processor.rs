28a63c10db895fb75774ed1452807745771d5493
darrexx <darx.dev@gmx.de>
2022-11-06 16:27:43 +0100

    Delete File after successful upload

--- src/document_processor.rs
+++ src/document_processor.rs
@@ -1,6 +1,5 @@
 use std::{fs, path::PathBuf, time::Duration};
 
-use anyhow::anyhow;
 use reqwest::{
     blocking::multipart::{Form, Part},
     IntoUrl,
@@ -25,7 +24,7 @@ where
 
     let client = reqwest::blocking::Client::new(); //Todo change to non blocking
 
-    let body = build_body(path)?;
+    let body = build_body(&path)?;
 
     let res = client
         .post(posturl)
@@ -39,22 +38,15 @@ where
         .log_if_error("Paperless return failure Status Code")?;
 
     log::info!("Successfully uploaded Document! {}", res.status());
+
+    fs::remove_file(path).log_if_error("Error deleting uploaded Document")?;
+
     Ok(res)
 }
 
-fn build_body(path: PathBuf) -> Result<Form, anyhow::Error> {
-    let content = fs::read(&path).log_if_error("Could not read file to be uploaded")?;
-
-    let filename = path
-        .file_name()
-        .ok_or_else(|| anyhow!("could not read filename of path"))
-        .log_if_error(format!("could not read filename of path {:?}", path))?
-        .to_os_string()
-        .into_string()
-        .map_err(|_| anyhow!("Could not conver OsString to String"))
-        .log_if_error("Could not conver OsString to String")?;
-
-    let file_part = Part::bytes(content).file_name(filename); //change to use stream
+fn build_body(path: &PathBuf) -> Result<Form, anyhow::Error> {
+    let file_part =
+        Part::file(path).log_if_error("Could not create file part of fiel to be uploaded")?;
     let form = Form::new();
     Ok(form.part("document", file_part))
 }
